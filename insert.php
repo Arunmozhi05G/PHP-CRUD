7bc300ca002e5fb98f0ded571631011099923531
Arunmozhi05G <arunmozhi3010@gmail.com>
2024-07-09 09:55:56 -0500

    session

--- insert.php
+++ insert.php
@@ -1,78 +1,78 @@
+<?php
+session_start();
+if (!isset($_SESSION["login"])) {
+    header("location: login.php");
+    exit();
+}
+
+$connection = mysqli_connect("localhost", "root", "", "admission");
+
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$query = "SELECT * FROM stud";
+$res = mysqli_query($connection, $query);
+
+if (!$res) {
+    die("Query failed: " . mysqli_error($connection));
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Admission</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Insert Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card-header">
-                     <h1 class="intab">Student Details</h1>
-                </div>
-                <div class="card-body">
-                    <button class="btn btn-success"><a href="add.php" class="text-light">Add Details</a></button>
-                    <button class="btn btn-danger"><a href="login.php" class="text-light">Log Out</a></button>
-                    <br/>
-                    <table class="table table-hover table-bordered table-striped">
-                        <thead>
-                            <tr>
-                                <th scope="col">No</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Age</th>
-                                <th scope="col">District</th>
-                                <th scope="col">Gender</th>
-                                <th scope="col">Hobbies</th>
-                                <th scope="col">Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            $connection = mysqli_connect("localhost", "root", "", "admission");
-
-                            if (!$connection) {
-                                die("Connection failed: " . mysqli_connect_error());
-                            }
-
-                            $sql = "SELECT * FROM stud";
-                            $run = mysqli_query($connection, $sql);
-
-                            if ($run) {
-                                while ($row = mysqli_fetch_array($run)) {
-                                    $id = $row['sno'];
-                                    $name = $row['name'];
-                                    $age = $row['age'];
-                                    $address = $row['address'];
-                                    $gender = $row['gender'];
-                                    $hobbies = $row['hobbies'];
-                                    echo "<tr>
-                                        <td>{$id}</td>
-                                        <td>{$name}</td>
-                                        <td>{$age}</td>
-                                        <td>{$address}</td>
-                                        <td>{$gender}</td>
-                                        <td>{$hobbies}</td>
-                                        <td>
-                                            <button class='btn btn-success'><a href='edit.php?edit={$id}' class='text-light'>Edit</a></button>
-                                            <button class='btn btn-danger'><a href='delete.php?del={$id}' class='text-light'>Delete</a></button>
-                                        </td>
-                                    </tr>";
-                                }
-                            } else {
-                                echo "<tr><td colspan='6'>No records found</td></tr>";
-                            }
-
-                            mysqli_close($connection);
-                            ?>
-                        </tbody>
-                    </table>
-                </div>
+<div class="container my-5">
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+            <div class="card-header">
+                <h1 class="intab">Student List</h1>
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>S.No</th>
+                            <th>Name</th>
+                            <th>Age</th>
+                            <th>Address</th>
+                            <th>Gender</th>
+                            <th>Hobbies</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php while ($row = mysqli_fetch_array($res)): ?>
+                        <tr>
+                            <td><?php echo $row['sno']; ?></td>
+                            <td><?php echo $row['name']; ?></td>
+                            <td><?php echo $row['age']; ?></td>
+                            <td><?php echo $row['address']; ?></td>
+                            <td><?php echo $row['gender']; ?></td>
+                            <td><?php echo $row['hobbies']; ?></td>
+                            <td>
+                                <a href="edit.php?edit=<?php echo $row['sno']; ?>" class="btn btn-success">Edit</a>
+                                <a href="delete.php?del=<?php echo $row['sno']; ?>" class="btn btn-danger">Delete</a>
+                            </td>
+                        </tr>
+                        <?php endwhile; ?>
+                    </tbody>
+                </table>
+                <a href="add.php" class="btn btn-primary">Add New Student</a>
+                <a href="login.php" class="btn btn-danger">Log Out</a>
             </div>
         </div>
+        <div class="col-md-2"></div>
     </div>
+</div>
 </body>
 </html>
--- register.php
+++ register.php
@@ -1,24 +1,25 @@
 <?php
-session_start();
-$connection = mysqli_connect("localhost", "root", "", "admission");
-
 if (isset($_POST['submit'])) {
+    $connection = mysqli_connect("localhost", "root", "", "admission");
+
+    if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $res = mysqli_query($connection, "SELECT * FROM login WHERE email = '$email'");
-    $row = mysqli_fetch_assoc($res);
+    
+    $stmt = $connection->prepare("INSERT INTO login (email, password) VALUES (?, ?)");
+    $stmt->bind_param("ss", $email, $password);
 
-    if (mysqli_num_rows($res) > 0) {
-        if ($password == $row["password"]) {
-            $_SESSION["login"] = true;
-            $_SESSION["id"] = $row["id"];
-            header("location: insert.php");
-        } else {
-            echo "<script> alert('Wrong password');</script>";
-        }
+    if ($stmt->execute()) {
+        echo '<script> location.replace("login.php")</script>';
     } else {
-        echo "<script> alert('User not registered');</script>";
+        echo "Something went wrong: " . $stmt->error;
     }
+
+    $stmt->close();
+    mysqli_close($connection);
 }
 ?>
 
@@ -27,20 +28,9 @@ if (isset($_POST['submit'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
-    <script>
-        function validateForm() {
-            var email = document.forms["loginForm"]["email"].value;
-            var password = document.forms["loginForm"]["password"].value;
-            if (email == "" || password == "") {
-                alert("Both email and password must be filled out");
-                return false;
-            }
-            return true;
-        }
-    </script>
+    <title>Registration Page</title>
 </head>
 <body>
 <div class="container my-5">
@@ -48,10 +38,10 @@ if (isset($_POST['submit'])) {
         <div class="col-md-4"></div>
         <div class="col-md-4">
             <div class="card-header">
-                <h1 class="intab">Login</h1>
+                <h1 class="intab">Registration</h1>
             </div>
             <div class="card-body">
-                <form name="loginForm" method="post" onsubmit="return validateForm()">
+                <form name="Form" method="post">
                     <div class="form-group">
                         <label>Email</label>
                         <input type="email" name="email" class="form-control" placeholder="Enter your email ID" required>
@@ -62,9 +52,8 @@ if (isset($_POST['submit'])) {
                     </div>
                     <br/>
                     <center>
-                        <input type="submit" class="btn btn-primary" name="submit" value="Login">
+                        <input type="submit" class="btn btn-primary" name="submit" value="Register">
                     </center>
-                    <a href="register.php">Register Your ID</a>
                 </form>
             </div>
         </div>
